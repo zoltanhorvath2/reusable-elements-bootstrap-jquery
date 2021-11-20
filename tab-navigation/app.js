d509944e073ee78f86532b02b77abb74e394d958
Zoltan Horvath <zoltan.horvath@proxybit.hu>
2021-11-20 17:37:11 +0100

    add final form of tabnav

--- tab-navigation/app.js
+++ tab-navigation/app.js
@@ -1,20 +1,18 @@
 $(function(){
 
-  $(document).on('click', '.nav-item', function(){
+  $('#menu1').tab('show');
 
-    //Showing selected tabs on edit location
-    if($(".nav-item .nav-link").attr('id') === 'menu1'){
-        console.log(1);
-        $('#menu1').tab('show');
-    }else if($(".nav-item .nav-link").attr('id') === 'menu2'){
-        console.log(2);
-        $('#menu2').tab('show');
-    }else if($(".nav-item .nav-link").attr('id') === 'menu3'){
-        console.log(3);
-        $('#menu3').tab('show');
-    }else{
-        console.log(4);
-        $('#menu4').tab('show');
+  $(document).on('click', '.nav-item .nav-link', function(){
+
+    switch($(this).attr('id')){
+      case 'menu4' : $('#menu4').tab('show');
+      break;
+      case 'menu2' : $('#menu2').tab('show');
+      break;
+      case 'menu3' : $('#menu3').tab('show');
+      break;
+      default: $('#menu1').tab('show');
+      break;
     }
 
   })
